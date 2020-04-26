8c06418fe719c3c7f54b4500bcc5ae7c4a15423d
HarshVardhan04 <62735790+HarshVardhan04@users.noreply.github.com>
2020-04-26 12:54:08 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,21 @@
+<?php
+
+
+if (isset($_POST['submit'])){
+   $name = $_POST['name']
+   $subject = $_POST['subject'];
+   $mailFrom = $_POST['mail'];
+   $messsage = $_POST['message'];
+   
+   
+   
+   
+   $mailTo = 'harshvardhandutta@gmail.com';
+   $headers = "From: ".$mailFrom;
+   $txt = "You have received an e-mail from ".$name."./n/n".$messsage;
+   mail($mailTo, $subject, $txt, $headers);
+  
+   header("Location: index.html?mailsend") 
+}
+  
+?>
